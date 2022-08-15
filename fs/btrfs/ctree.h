b976b405f1c3f93a274e582b16e941ca120f74cb
Boris Burkov <boris@bur.io>
2022-08-15 13:54:28 -0700

    btrfs: send: add support for fs-verity
    Preserve the fs-verity status of a btrfs file across send/recv.
    
    There is no facility for installing the Merkle tree contents directly on
    the receiving filesystem, so we package up the parameters used to enable
    verity found in the verity descriptor. This gives the receive side
    enough information to properly enable verity again. Note that this means
    that receive will have to re-compute the whole Merkle tree, similar to
    how compression worked before encoded_write.
    
    Since the file becomes read-only after verity is enabled, it is
    important that verity is added to the send stream after any file writes.
    Therefore, when we process a verity item, merely note that it happened,
    then actually create the command in the send stream during
    'finish_inode_if_needed'.
    
    This also creates V3 of the send stream format, without any format
    changes besides adding the new commands and attributes.
    
    Signed-off-by: Boris Burkov <boris@bur.io>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -4105,6 +4105,7 @@ static inline int btrfs_defrag_cancelled(struct btrfs_fs_info *fs_info)
 
 extern const struct fsverity_operations btrfs_verityops;
 int btrfs_drop_verity_items(struct btrfs_inode *inode);
+int btrfs_get_verity_descriptor(struct inode *inode, void *buf, size_t buf_size);
 
 BTRFS_SETGET_FUNCS(verity_descriptor_encryption, struct btrfs_verity_descriptor_item,
 		   encryption, 8);
@@ -4120,6 +4121,12 @@ BTRFS_SETGET_STACK_FUNCS(stack_verity_descriptor_size,
 static inline int btrfs_drop_verity_items(struct btrfs_inode *inode)
 {
 	return 0;
+}
+
+static inline int btrfs_get_verity_descriptor(struct inode *inode, void *buf,
+					      size_t buf_size)
+{
+	return -EPERM;
 }
 
 #endif
